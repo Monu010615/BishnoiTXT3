5447941d560dfbc8dc4638e8803a18c6e0642c00
Monu010615 <monu010615@gmail.com>
2024-06-02 22:17:07 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,4 +1,4 @@
 api_id = "26395620"
 api_hash = "906cc7e4b4c1ad7f7121efc2244d13ad"
-bot_token = "7199655119:AAGf5voAuXaGP7frVXec5PRdd5PjUket5Mw"
-auth_groups = "-1004252687833"
+bot_token = "7241048564:AAHmFfIWODaqf0-pYscCJwF6jUlaYGN6ecw"
+auth_groups = "-1002103286911"
